1fb589d9d2f4d8378a89224f2d4fefdfa54cf5e4
Justin Littman <justinlittman@stanford.edu>
2020-06-08 21:52:45 -0600

    Fix lambda. Fix submit button on reload.

--- lambda/lambda_function.rb
+++ lambda/lambda_function.rb
@@ -7,9 +7,9 @@ require 'rdf2marc'
 
 def lambda_handler(event:, context:)
   instance_uri = event['queryStringParameters']['instance']
-  instance_graph, work_graph, admin_metadata_graph = Rdf2marc::GraphsLoader.from_instance_uri(instance_uri)
+  graph, instance_term, work_term, admin_metadata_term = Rdf2marc::GraphsLoader.from_instance_uri(instance_uri)
 
-  record_model = Rdf2marc::Rdf2model.to_model(instance_graph, work_graph, admin_metadata_graph)
+  record_model = Rdf2marc::Rdf2model.to_model(graph, instance_term, work_term, admin_metadata_term)
   marc_record = Rdf2marc::Model2marc::Record.new(record_model)
   response(200, marc_record.to_s)
 rescue Rdf2marc::UnhandledError => e
@@ -22,10 +22,10 @@ end
 
 def response(code, body)
   {
-    'statusCode' => code,
-    'headers' => {
-      'Content-Type' => 'text/plain'
-    },
-    'body' => body
+      'statusCode' => code,
+      'headers' => {
+          'Content-Type' => 'text/plain'
+      },
+      'body' => body
   }
 end

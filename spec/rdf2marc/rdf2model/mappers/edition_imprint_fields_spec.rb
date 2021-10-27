729991e8ba1001aed686d54e6e3d15002f3f2fca
Justin Coyne <jcoyne@justincoyne.com>
2021-10-27 11:57:40 -0500

    Add the ability to handle literal agent on imprint fields
    Fixes #127

--- spec/rdf2marc/rdf2model/mappers/edition_imprint_fields_spec.rb
+++ spec/rdf2marc/rdf2model/mappers/edition_imprint_fields_spec.rb
@@ -28,62 +28,87 @@ RSpec.describe Rdf2marc::Rdf2model::Mappers::EditionImprintFields, :vcr do
   end
 
   describe 'publication_distributions' do
-    let(:ttl) do
-      <<~TTL
-                <#{instance_term}> <http://id.loc.gov/ontologies/bibframe/provisionActivity> _:b11 .
-        _:b11 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://id.loc.gov/ontologies/bibframe/Publication> .
-        _:b11 <http://id.loc.gov/ontologies/bibframe/place> <http://id.loc.gov/authorities/names/n82070796> .
-        <http://id.loc.gov/authorities/names/n82070796> <http://www.w3.org/2000/01/rdf-schema#label> "Freehold (N.J.)" .
-        _:b11 <http://id.loc.gov/ontologies/bibframe/agent> <http://id.loc.gov/authorities/names/n82158730> .
-        <http://id.loc.gov/authorities/names/n82158730> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://id.loc.gov/ontologies/bibframe/Agent> .
-        <http://id.loc.gov/authorities/names/n82158730> <http://www.w3.org/2000/01/rdf-schema#label> "ABC-Clio Information Services" .
-        <http://id.loc.gov/authorities/names/n82158730> <http://id.loc.gov/ontologies/bibframe/role> <http://id.loc.gov/vocabulary/relators/pbl> .
-        <http://id.loc.gov/vocabulary/relators/pbl> <http://www.w3.org/2000/01/rdf-schema#label> "Publisher" .
-        _:b11 <http://id.loc.gov/ontologies/bibframe/date> "2020"@eng .
-        <#{instance_term}> <http://id.loc.gov/ontologies/bibframe/provisionActivity> _:b13 .
-        _:b13 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://id.loc.gov/ontologies/bibframe/Distribution> .
-        _:b13 <http://id.loc.gov/ontologies/bibframe/place> <http://id.loc.gov/authorities/names/n00068607> .
-        <http://id.loc.gov/authorities/names/n00068607> <http://www.w3.org/2000/01/rdf-schema#label> "West Seattle (Wash.)" .
-        _:b13 <http://id.loc.gov/ontologies/bibframe/agent> <http://id.loc.gov/authorities/names/no2003107987> .
-        <http://id.loc.gov/authorities/names/no2003107987> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://id.loc.gov/ontologies/bibframe/Agent> .
-        <http://id.loc.gov/authorities/names/no2003107987> <http://www.w3.org/2000/01/rdf-schema#label> "Wood and Iverson Lumber Company" .
-        _:b13 <http://id.loc.gov/ontologies/bibframe/date> "2019"@eng .
-        <#{instance_term}> <http://id.loc.gov/ontologies/bibframe/provisionActivity> _:b15 .
-        _:b15 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://id.loc.gov/ontologies/bibframe/Manufacture> .
-        _:b15 <http://id.loc.gov/ontologies/bibframe/place> <http://id.loc.gov/authorities/names/n79061258> .
-        <http://id.loc.gov/authorities/names/n79061258> <http://www.w3.org/2000/01/rdf-schema#label> "Cambridge (Mass.)" .
-        _:b15 <http://id.loc.gov/ontologies/bibframe/agent> <http://id.loc.gov/authorities/names/nr92038535> .
-        <http://id.loc.gov/authorities/names/nr92038535> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://id.loc.gov/ontologies/bibframe/Agent> .
-        <http://id.loc.gov/authorities/names/nr92038535> <http://www.w3.org/2000/01/rdf-schema#label> "Cambridge Press (Cambridge, Mass.)" .
-        _:b15 <http://id.loc.gov/ontologies/bibframe/date> "2017"@eng .
-      TTL
+    context 'with lookup' do
+      let(:model) do
+        {
+          publication_distributions: [
+            {
+              entity_function: 'publication',
+              publication_distribution_places: ['Freehold (N.J.)'],
+              publisher_distributor_names: ['ABC-Clio Information Services'],
+              publication_distribution_dates: ['2020']
+            },
+            {
+              entity_function: 'publication',
+              publication_distribution_places: ['West Seattle (Wash.)'],
+              publisher_distributor_names: ['Wood and Iverson Lumber Company'],
+              publication_distribution_dates: ['2019']
+            },
+            {
+              entity_function: 'manufacture',
+              publication_distribution_places: ['Cambridge (Mass.)'],
+              publisher_distributor_names: ['Cambridge Press (Cambridge, Mass.)'],
+              publication_distribution_dates: ['2017']
+            }
+          ]
+        }
+      end
+      let(:ttl) do
+        <<~TTL
+                  <#{instance_term}> <http://id.loc.gov/ontologies/bibframe/provisionActivity> _:b11 .
+          _:b11 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://id.loc.gov/ontologies/bibframe/Publication> .
+          _:b11 <http://id.loc.gov/ontologies/bibframe/place> <http://id.loc.gov/authorities/names/n82070796> .
+          <http://id.loc.gov/authorities/names/n82070796> <http://www.w3.org/2000/01/rdf-schema#label> "Freehold (N.J.)" .
+          _:b11 <http://id.loc.gov/ontologies/bibframe/agent> <http://id.loc.gov/authorities/names/n82158730> .
+          <http://id.loc.gov/authorities/names/n82158730> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://id.loc.gov/ontologies/bibframe/Agent> .
+          <http://id.loc.gov/authorities/names/n82158730> <http://www.w3.org/2000/01/rdf-schema#label> "ABC-Clio Information Services" .
+          <http://id.loc.gov/authorities/names/n82158730> <http://id.loc.gov/ontologies/bibframe/role> <http://id.loc.gov/vocabulary/relators/pbl> .
+          <http://id.loc.gov/vocabulary/relators/pbl> <http://www.w3.org/2000/01/rdf-schema#label> "Publisher" .
+          _:b11 <http://id.loc.gov/ontologies/bibframe/date> "2020"@eng .
+          <#{instance_term}> <http://id.loc.gov/ontologies/bibframe/provisionActivity> _:b13 .
+          _:b13 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://id.loc.gov/ontologies/bibframe/Distribution> .
+          _:b13 <http://id.loc.gov/ontologies/bibframe/place> <http://id.loc.gov/authorities/names/n00068607> .
+          <http://id.loc.gov/authorities/names/n00068607> <http://www.w3.org/2000/01/rdf-schema#label> "West Seattle (Wash.)" .
+          _:b13 <http://id.loc.gov/ontologies/bibframe/agent> <http://id.loc.gov/authorities/names/no2003107987> .
+          <http://id.loc.gov/authorities/names/no2003107987> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://id.loc.gov/ontologies/bibframe/Agent> .
+          <http://id.loc.gov/authorities/names/no2003107987> <http://www.w3.org/2000/01/rdf-schema#label> "Wood and Iverson Lumber Company" .
+          _:b13 <http://id.loc.gov/ontologies/bibframe/date> "2019"@eng .
+          <#{instance_term}> <http://id.loc.gov/ontologies/bibframe/provisionActivity> _:b15 .
+          _:b15 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://id.loc.gov/ontologies/bibframe/Manufacture> .
+          _:b15 <http://id.loc.gov/ontologies/bibframe/place> <http://id.loc.gov/authorities/names/n79061258> .
+          <http://id.loc.gov/authorities/names/n79061258> <http://www.w3.org/2000/01/rdf-schema#label> "Cambridge (Mass.)" .
+          _:b15 <http://id.loc.gov/ontologies/bibframe/agent> <http://id.loc.gov/authorities/names/nr92038535> .
+          <http://id.loc.gov/authorities/names/nr92038535> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://id.loc.gov/ontologies/bibframe/Agent> .
+          <http://id.loc.gov/authorities/names/nr92038535> <http://www.w3.org/2000/01/rdf-schema#label> "Cambridge Press (Cambridge, Mass.)" .
+          _:b15 <http://id.loc.gov/ontologies/bibframe/date> "2017"@eng .
+        TTL
+      end
+
+      include_examples 'mapper', described_class
     end
 
-    let(:model) do
-      {
-        publication_distributions: [
-          {
-            entity_function: 'publication',
-            publication_distribution_places: ['Freehold (N.J.)'],
-            publisher_distributor_names: ['ABC-Clio Information Services'],
-            publication_distribution_dates: ['2020']
-          },
-          {
-            entity_function: 'publication',
-            publication_distribution_places: ['West Seattle (Wash.)'],
-            publisher_distributor_names: ['Wood and Iverson Lumber Company'],
-            publication_distribution_dates: ['2019']
-          },
-          {
-            entity_function: 'manufacture',
-            publication_distribution_places: ['Cambridge (Mass.)'],
-            publisher_distributor_names: ['Cambridge Press (Cambridge, Mass.)'],
-            publication_distribution_dates: ['2017']
-          }
-        ]
-      }
-    end
+    context 'with literal' do
+      let(:model) do
+        {
+          publication_distributions: [
+            {
+              entity_function: 'publication',
+              publisher_distributor_names: ['ABC-Clio Information Services']
+            }
+          ]
+        }
+      end
+      let(:ttl) do
+        <<~TTL
+                  <#{instance_term}> <http://id.loc.gov/ontologies/bibframe/provisionActivity> _:b11 .
+          _:b11 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://id.loc.gov/ontologies/bibframe/Publication> .
+          _:b11 <http://id.loc.gov/ontologies/bibframe/agent> _:b12 .
+          _:b12 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://id.loc.gov/ontologies/bibframe/Agent> .
+          _:b12 <http://www.w3.org/2000/01/rdf-schema#label> "ABC-Clio Information Services" .
+        TTL
+      end
 
-    include_examples 'mapper', described_class
+      include_examples 'mapper', described_class
+    end
   end
 end
